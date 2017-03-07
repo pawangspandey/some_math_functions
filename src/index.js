4500b9a97c2c900a8f24eb25069c97ab85de5b3c
pawan pandey <pawan4zip@gmail.com>
2017-03-07 18:33:29 +0530

    added coverage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var geolib = require('geolib');
 module.exports = {
     isPointInsideCircle : function(point,center,radius){
         if(typeof point !== 'object' && typeof point !== 'object'){
-            throw Error("point and center should be object");
+            throw new Error("point and center should be object");
         }else{
             return geolib.isPointInCircle(point,center,radius);
         }
--- src/test/unit/index.test.js
+++ src/test/unit/index.test.js
@@ -4,5 +4,15 @@ var lib = require('../../index.js');
 describe('some math functions api', function() {
     it('Should have isPointInsideCircle function',function(){
         expect(lib).to.have.property('isPointInsideCircle');
-    })
+    });
+    
+    describe('isPointInsideCircle api',function(){
+        it('Should throw exception point and center should be object',function(){
+            expect(lib.isPointInsideCircle.bind(lib,"some bad thing","some bad ","bad")).to.throw('point and center should be object');
+        });
+
+        it('Should return boolean value',function(){
+            expect(lib.isPointInsideCircle({latitude:19.1344,longitude:73.1445},{latitude:19.1344,longitude:73.1445},500)).to.a('boolean');
+        });        
+    });
 })
